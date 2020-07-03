fixed non-flowing gradient

agent/agent.py
@@ -42,40 +42,38 @@ class Exerminator:
         self.current_state = next_state
         if len(self.memory) > self.hparams.mem_size:
             self.memory.popleft()
-        self.train()
 
     def pick_action(self):
-        with torch.no_grad():
-            action_prob = self.actor(self.current_state) # , self.current_action)
-            assert action_prob.size() == (1, self.env_side_len * self.env_side_len)
-            self.current_action = torch.round(action_prob).reshape((1, 1, self.env_side_len, self.env_side_len))
+        # with torch.no_grad():
+        action_prob = self.actor(self.current_state).detach() # , self.current_action)
+        assert action_prob.size() == (1, self.env_side_len * self.env_side_len)
+        self.current_action = torch.round(action_prob).reshape((1, 1, self.env_side_len, self.env_side_len))
         return self.current_action.cpu().numpy()
 
     def train(self):
         if len(self.memory) > self.hparams.batch_sz:
             states, actions, rewards, done, next_states = self.get_sample()
-            next_actions = self.actor(next_states)
-            next_values = self.critic(next_states)
+            next_values = self.critic(next_states).detach()
             td_targets = rewards + self.hparams.gamma * next_values
             if td_targets.dim() < 2:
                 td_targets.reshape_((-1, 1))
 
             # Train critic
             value_loss = self.critic.training_step((states, td_targets))
+            print(f"Updated Critic, value loss = {value_loss:.5f}")
 
             # Train actor
-            with torch.no_grad():
-                current_qs = self.critic(states)
-                td_errors = td_targets - current_qs
+            # with torch.no_grad():
+            current_qs = self.critic(states)
+            td_errors = td_targets - current_qs
             policy_loss = self.actor.training_step((states, td_errors))
-
+            print(f"Updated Actor, policy loss = {policy_loss:.5f}")
 
 """
 Notes:
 agent_hparams:
     mem_size: Size of replay buffer (default = 10000 ?)
     gamma: discount rate (default = 0.99 ?)
-    steps: num. steps in an episode (default = 300 ?)
     device: 'cpu' or 'gpu'
     batch_sz: size of batches from replay buffer
     

environment/environment.py
@@ -76,11 +76,14 @@ class InvasiveEnv:
         if self.reward_method == 'sum':
             reward = -np.sum(self.grid).astype(np.float32) - cost
         else:  # 'count'
-            reward = -np.count_nonzero(self.grid).astype(np.float32) - cost
+            reward = -float(np.count_nonzero(self.grid)) - cost
         return reward
 
     def __repr__(self):
-        return f'Environment with {self.grid.sum()} individuals:\n{self.grid}'
+        if self.grid:
+            return f'Environment with {self.grid.sum()} individuals:\n{self.grid}'
+        else:
+            return 'Undefined environment. Run .reset() first.'
 
 
 if __name__ == '__main__':

models/actor_model.py
@@ -51,7 +51,7 @@ class Actor(nn.Module):
     def _configure_optimiser(self):
         return optim.AdamW(self.parameters(), lr = self.hparams.lr, weight_decay = self.hparams.wd)
 
-    def forward(self, states,):
+    def forward(self, states):
         h_states = self.state_features(states)
         # h_actions = self.action_features(actions)
         return self.classifier(h_states # + h_actions
@@ -64,7 +64,7 @@ class Actor(nn.Module):
         #          = -log(pi(a_1|state) * pi(a_2|state) * ... * pi(a_n|state)) * td_error
         #          = -td_error * sum(log(pi(a_i|state)))  ?? hopefully
         # but clamp pi(*) values above some small constant to avoid zero everywhere
-        loss_val = -(td_errors * torch.sum(torch.log(action_probs), dim=1)).mean()
+        loss_val = (td_errors * torch.sum(torch.log(action_probs), dim=1)).mean()
         return loss_val
 
     def training_step(self, batch):
@@ -73,11 +73,11 @@ class Actor(nn.Module):
         # if states.dim() == 2: states = states.unsqueeze(0)
         # if td_errors.dim() == 1: td_errors = td_errors.unsqueeze(0)
         self.optim.zero_grad()
-        with torch.enable_grad():
-            action_probs = self(states) # , actions)
-            loss_value = self.loss_fn(td_errors, action_probs)
-            loss_value.backward()
-            self.optim.step()
+        # with torch.enable_grad():
+        action_probs = self(states) # , actions)
+        loss_value = self.loss_fn(td_errors, action_probs)
+        loss_value.backward()
+        self.optim.step()
         return loss_value
 
 

models/critic_model.py
@@ -44,7 +44,6 @@ class Critic(nn.Module):
         return self.regressor(h)
 
     def loss_fn(self, td_targets, value_estimates):
-
         return torch.mean((td_targets - value_estimates) ** 2)
 
     def training_step(self, batch):
@@ -52,11 +51,11 @@ class Critic(nn.Module):
         # if states.dim() == 2: states.unsqueeze_(0)
         # if td_targets.dim() == 1: td_targets.unsqueeze_(0)
         self.optim.zero_grad()
-        with torch.enable_grad():
-            value_estimates = self(states)
-            loss_value = self.loss_fn(td_targets, value_estimates)
-            loss_value.backward()
-            self.optim.step()
+        # with torch.enable_grad():
+        value_estimates = self(states)
+        loss_value = self.loss_fn(td_targets, value_estimates)
+        loss_value.backward()
+        self.optim.step()
         return loss_value
 
 

training/training.py
@@ -17,6 +17,7 @@ def run_episode(agent, env, n_steps, ep_idx, initial_state = None):
         # next_state = torch.tensor(next_state, device=device)
         # reward = torch.tensor(reward, device=device)
         agent.observe(next_state, reward, is_terminal)
+        agent.train()
         score += reward
         if is_terminal:
             break
@@ -26,7 +27,7 @@ def run_episode(agent, env, n_steps, ep_idx, initial_state = None):
 
 def train(n_episodes,
           env_hparams, agent_hparams, actor_hparams, critic_hparams,
-          n_steps=300, initial_state = None):
+          n_steps = 300, initial_state = None):
     env = InvasiveEnv(env_hparams, initial_state)
     agent = Exerminator(env.side_len, actor_hparams, critic_hparams, agent_hparams)
     episode_lens = []